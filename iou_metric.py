a2732159df3ee193de58dbe239bf872cdbc5c23a
Balu Purohit <balupurohit23@gmail.com>
2018-03-30 19:43:50 +0530

    Create iou_metric.py

---
+++ iou_metric.py
@@ -0,0 +1,30 @@
+def iou_loss(y_true, y_pred):
+    # iou as metric for bounding box regression
+    # input must be as [x1, y1, x2, y2]
+    
+    # AOG = Area of Groundtruth box
+    AoG = K.abs(K.transpose(y_true)[2] - K.transpose(y_true)[0] + 1) * K.abs(K.transpose(y_true)[3] - K.transpose(y_true)[1] + 1)
+    
+    # AOP = Area of Predicted box
+    AoP = K.abs(K.transpose(y_pred)[2] - K.transpose(y_pred)[0] + 1) * K.abs(K.transpose(y_pred)[3] - K.transpose(y_pred)[1] + 1)
+
+    # overlaps are the co-ordinates of intersection box
+    overlap_0 = K.maximum(K.transpose(y_true)[0], K.transpose(y_pred)[0])
+    overlap_1 = K.maximum(K.transpose(y_true)[1], K.transpose(y_pred)[1])
+    overlap_2 = K.minimum(K.transpose(y_true)[2], K.transpose(y_pred)[2])
+    overlap_3 = K.minimum(K.transpose(y_true)[3], K.transpose(y_pred)[3])
+
+    # intersection area
+    intersection = (overlap_2 - overlap_0 + 1) * (overlap_3 - overlap_1 + 1)
+
+    # area of union of both boxes
+    union = AoG + AoP - intersection
+    
+    # iou calculation
+    iou = intersection / union
+
+    # bounding values of iou to (0,1)
+    iou = K.clip(iou, 0.0 + K.epsilon(), 1.0 - K.epsilon())
+
+    return iou_loss
+    
